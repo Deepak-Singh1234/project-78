01654b5ea2ffe5d9239075c7c21d0540b79b8a67
Deepak-Singh1234 <71645902+Deepak-Singh1234@users.noreply.github.com>
2021-05-17 12:10:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var images = ["th.jpg","685.jpg.jpg","889.jpg","20210517_114902.jpg","IMG-20210517-WA0003"]
+var i=0;
+
+function family(){
+  document.getElementById("htmgg").src=images[i];
+  i++;
+}
